d2f01646c150531fe27dca57c224572a06508dad
bretek <josephmadden999@gmail.com>
2025-08-23 12:44:10 +0100

    feat: added file with imperative setup commands (secret key imports, secure boot, tpm)

---
+++ imperative-setup.sh
@@ -0,0 +1,10 @@
+# secure boot
+sudo sbctl create-keys
+sudo sbctl enroll-keys --microsoft
+
+# tpm disk unlock
+sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=1+3+5+7+11+12+14+15 --wipe-slot=tpm2 /dev/sda2
+
+# secret keys
+gpg --import <gpg-secret>
+cp <ssh-secret> ~/.ssh/<ssh-secret>
