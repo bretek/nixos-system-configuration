bc9d20abc2ad593577b9a1a37e56cca5e2d8d759
bretek <josephmadden999@gmail.com>
2026-07-15 14:07:30 +0100

    task: set linux kernel version

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@ in
 
   boot.initrd.systemd.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_7_0;
 
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
