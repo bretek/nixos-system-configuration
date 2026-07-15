4c703a1217611a9d1cd8d0912fc77494db999149
bretek <josephmadden999@gmail.com>
2026-07-15 14:32:36 +0100

    task(nvim): switch C# LSP to csharp_ls

--- users/joseph/home/programs/nvim/plugins/lsp/csharp.nix
+++ users/joseph/home/programs/nvim/plugins/lsp/csharp.nix
@@ -1,14 +1,10 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    pkgs.roslyn-ls
-  ];
-
   programs.nixvim.plugins = {
     treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
       c_sharp
     ];
 
-    lsp.servers.roslyn_ls.enable = true;
+    lsp.servers.csharp_ls.enable = true;
   };
 }
