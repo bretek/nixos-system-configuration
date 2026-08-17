1a7cb1a943de55c0f48eb122f7c3131b80156f3f
bretek <josephmadden999@gmail.com>
2026-08-17 17:57:24 +0100

    task(nvim): ensure telescope has ripgrep pkg available

--- users/joseph/home/programs/nvim/plugins/telescope.nix
+++ users/joseph/home/programs/nvim/plugins/telescope.nix
@@ -1,24 +1,27 @@
+{ pkgs, ... }:
 {
+  home.packages = [ pkgs.ripgrep ];
+
   programs.nixvim.plugins.telescope = {
-      enable = true;
+    enable = true;
 
-      keymaps = {
-        # Find files using Telescope command-line sugar.
-        "<leader>fp" = "find_files";
-        "<leader>fg" = "live_grep";
-        "<leader>fh" = "help_tags";
-        "<leader>fd" = "diagnostics";
+    keymaps = {
+      # Find files using Telescope command-line sugar.
+      "<leader>fp" = "find_files";
+      "<leader>fg" = "live_grep";
+      "<leader>fh" = "help_tags";
+      "<leader>fd" = "diagnostics";
 
-        # FZF like bindings
-        "<C-p>" = "git_files";
-        "<leader>p" = "oldfiles";
-      };
+      # FZF like bindings
+      "<C-p>" = "git_files";
+      "<leader>p" = "oldfiles";
+    };
 
-      settings.defaults = {
-        set_env.COLORTERM = "truecolor";
-        file_ignore_patterns = [
-          "%__virtual.cs$"
-        ];
-      };
+    settings.defaults = {
+      set_env.COLORTERM = "truecolor";
+      file_ignore_patterns = [
+        "%__virtual.cs$"
+      ];
+    };
   };
 }
