ca0d7ad5c37a7944637b7e5cd75832ebdd3032dd
bretek <josephmadden999@gmail.com>
2026-08-17 17:58:04 +0100

    task(nvim): format plugins/default.nix

--- users/joseph/home/programs/nvim/plugins/default.nix
+++ users/joseph/home/programs/nvim/plugins/default.nix
@@ -1,13 +1,13 @@
 { ... }:
 {
-      imports = [
-          ./lsp
-          ./treesitter.nix
-          ./telescope.nix
-          ./ts-autotag.nix
-      ];
+  imports = [
+    ./lsp
+    ./treesitter.nix
+    ./telescope.nix
+    ./ts-autotag.nix
+  ];
 
-    programs.nixvim.plugins = {
-      web-devicons.enable = true;
-    };
+  programs.nixvim.plugins = {
+    web-devicons.enable = true;
+  };
 }
