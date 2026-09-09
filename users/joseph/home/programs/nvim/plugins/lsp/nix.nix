715a06a979a31fdd33a22c76e43ebe9f6fa7cf72
bretek <josephmadden999@gmail.com>
2026-09-09 22:56:09 +0100

    feat(nvim): nixd include home-manager options for autocompletion

--- users/joseph/home/programs/nvim/plugins/lsp/nix.nix
+++ users/joseph/home/programs/nvim/plugins/lsp/nix.nix
@@ -11,6 +11,22 @@
       pkgs.vimPlugins.nvim-treesitter.builtGrammars.nix
     ];
 
-    lsp.servers.nixd.enable = true;
+    lsp.servers.nixd = {
+      enable = true;
+      settings = {
+        options = {
+          home-manager = {
+            expr = ''
+              let
+                sources = import ${../../../../../../../npins};
+              in
+              (import (sources.nixpkgs + "/nixos") {
+                configuration = ${../../../../../../..}/configuration.nix;
+              }).options.home-manager.users.type.getSubOptions []
+            '';
+          };
+        };
+      };
+    };
   };
 }
