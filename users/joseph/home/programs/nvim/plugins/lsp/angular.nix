d882ddc272b344339cd833ca02a8183b65ccf756
bretek <josephmadden999@gmail.com>
2026-09-10 20:25:07 +0100

    fix(nvim): angularls use correct rootMarkers and attach

--- users/joseph/home/programs/nvim/plugins/lsp/angular.nix
+++ users/joseph/home/programs/nvim/plugins/lsp/angular.nix
@@ -9,6 +9,42 @@
       config.programs.nixvim.plugins.treesitter.package.builtGrammars.angular
     ];
 
-    lsp.servers.angularls.enable = true;
+    lsp.servers.angularls = {
+      enable = true;
+      rootMarkers = [
+        "angular.json"
+        "nx.json"
+      ];
+      extraOptions = {
+        cmd.__raw = ''
+          (function()
+            local probe_dir = require('lspconfig.util').find_node_modules_ancestor(vim.fn.getcwd())
+            probe_dir = probe_dir and (probe_dir .. '/node_modules') or '''
+            return {
+              "ngserver", "--stdio",
+              "--logFile", "/tmp/ngserver.log",
+              "--logVerbosity", "verbose",
+              "--tsProbeLocations", probe_dir,
+              "--ngProbeLocations", probe_dir,
+              "--forceStrictTemplates",
+            }
+          end)()
+        '';
+        on_new_config.__raw = ''
+          function(new_config, new_root_dir)
+            local probe_dir = require('lspconfig.util').find_node_modules_ancestor(new_root_dir)
+            probe_dir = probe_dir and (probe_dir .. '/node_modules') or '''
+            new_config.cmd = {
+              "ngserver", "--stdio",
+              "--logFile", "/tmp/ngserver.log",
+              "--logVerbosity", "verbose",
+              "--tsProbeLocations", probe_dir,
+              "--ngProbeLocations", probe_dir,
+              "--forceStrictTemplates",
+            }
+          end
+        '';
+      };
+    };
   };
 }
