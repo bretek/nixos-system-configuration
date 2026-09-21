04baeafedeff079ea3d75aa86a89a839685e7c7b
bretek <josephmadden999@gmail.com>
2026-09-21 23:56:54 +0100

    feat: add android-file-transfer and rquickshare

--- users/joseph/packages.nix
+++ users/joseph/packages.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 {
   users.users.joseph.packages = with pkgs; [
+    android-file-transfer
     anki
     crosswords
     gnome-sudoku
     inkscape
     mixxx
     nemo
+    rquickshare
   ];
 }
